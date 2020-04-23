77899d93bb11a5a6c0a3b7f428d96412272ad6e4
xiaofeng.zhi <xiaofeng.zhi@staff.smartline.com.au>
2020-04-23 10:35:00 +0800

    modify function

--- src/function/Test.kt
+++ src/function/Test.kt
@@ -6,12 +6,36 @@ fun printlen(str: String): Int {
     return str.length
 }
 
-// 如果函数只有一行，可以省略返回值
+// 如果函数体只有一个语句，可以将这个语句赋值给函数
 // 使用字符串模版拼接字符串
 fun sayHello(str: String) = printlen("hello $str")
 fun getAge() = 18
 
+// 函数默认值，当不穿参数是取默认值
+fun greet(str: String = "Tristan") {
+    println("hello $str")
+}
+
+// 函数嵌套
+// 通常使用在某些条件下触发递归函数，不希望被外部函数访问的函数
+// 一般情况下不推荐使用内部函数，会将代码可读性降低
+fun function() {
+    val str = "Hello World"
+
+    // 可以访问外部变量
+    fun say(count: Int = 10) {
+        println(str)
+        if (count > 0) {
+            say(count - 1)
+        }
+    }
+    say()
+}
+
 fun main(args: Array<String>) {
     printlen("Tristan")
     sayHello("Tristan")
+    // 不穿参数
+    greet()
+    function()
 }
